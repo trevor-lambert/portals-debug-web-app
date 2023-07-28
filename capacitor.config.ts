e79017f33f97688fe62f8ee5fef7ef14003f14b0
Trevor Lambert <trevor+production@ionic.io>
2023-07-28 18:32:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Portals Debug Web App',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
